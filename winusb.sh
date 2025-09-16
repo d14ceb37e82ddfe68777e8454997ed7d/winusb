644ab22686e8bc643403a04448380707e175ba43
d14ceb37e82ddfe68777e8454997ed7d <216149341+d14ceb37e82ddfe68777e8454997ed7d@users.noreply.github.com>
2025-09-16 21:53:51 +0800

    Update winusb.sh

--- winusb.sh
+++ winusb.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 echo "SHASUM1" | shasum --algorithm 256 --check && \
 echo "$2  $1" | shasum --algorithm 256 --check && \
-echo "Checksum OK"
+echo "Checksum OK" && \
+mkdir mnt && \
+hdiutil attach $1 -readonly -mountpoint mnt && \
+hdiutil detach mnt && \
+rmdir mnt && \
+echo "OK"
