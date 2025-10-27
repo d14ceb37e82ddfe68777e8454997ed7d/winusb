cac5c41ccffb4f56d61b8ce5158d68f13efe0d53
d14ceb37e82ddfe68777e8454997ed7d <216149341+d14ceb37e82ddfe68777e8454997ed7d@users.noreply.github.com>
2025-10-27 13:32:32 +0800

    Update winusb.sh

--- winusb.sh
+++ winusb.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-echo "SHASUM1" | shasum --algorithm 256 --check && \
-echo "$2  $1" | shasum --algorithm 256 --check && \
-echo "Checksum OK" && \
+echo "SHASUM1" | shasum --check && \
+echo "$2  $1" | shasum --check && \
+diskutil eraseDisk -noEFI FAT32 %noformat% GPT $3 && \
+sudo newfs_msdos -F 32 -v Windows $3s1 && \
+diskutil mount nobrowse $3s1 && \
 mkdir mnt && \
 hdiutil attach $1 -readonly -mountpoint mnt && \
-diskutil eraseDisk -noEFI FAT32 Windows GPT $3 && \
 rsync --archive --max-size 3800M mnt/ /Volumes/Windows/ && \
 xattr -c wimsplit && \
 ./wimsplit mnt/sources/install.wim /Volumes/Windows/sources/install.swm 3800 && \
-diskutil eject $3 && \
 hdiutil detach mnt && \
 rmdir mnt && \
+diskutil eject $3 && \
 echo "OK"
